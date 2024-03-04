6d91a830524041b04c969d1df5d20c84ca2c9a4b
Jardeson <jardeson777@gmail.com>
2024-03-04 20:06:03 -0300

    chore: ajustes de espaçamento no código

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -3,42 +3,39 @@ import { colors } from "../theme/colors"
 import { ReactNode } from "react"
 
 type CardProps = {
-    children: ReactNode;
-    variant?: 'primary'|'secondary';
+  children: ReactNode;
+  variant?: 'primary' | 'secondary';
 }
 
+export const Card = ({ children, variant = 'primary' }: CardProps) => {
+  const variantStyles = {
+    primary: styles.primary,
+    secondary: styles.secondary
+  }
 
-
-export const Card = ({children, variant='primary'}: CardProps) => {
-    
-    const variantStyles = {
-        primary: styles.primary,
-        secondary: styles.secondary
-    }
-
-    return (
-        <View style={variantStyles[variant]}>
-            {children}
-        </View>
-    )
+  return (
+    <View style={variantStyles[variant]}>
+      {children}
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
-    primary: {
-        width: '100%',
-        height: 'auto',
-        padding: 30,
-        backgroundColor: colors.white,
-        borderColor: colors.primary,
-        borderWidth: 1,
-        borderRadius: 8
-    },
-    secondary: {
-        width: '100%',
-        height: 'auto',
-        padding: 30,
-        backgroundColor: colors.white,
-        borderRadius: 8,
-        elevation: 4
-    }
+  primary: {
+    width: '100%',
+    height: 'auto',
+    padding: 30,
+    backgroundColor: colors.white,
+    borderColor: colors.primary,
+    borderWidth: 1,
+    borderRadius: 8
+  },
+  secondary: {
+    width: '100%',
+    height: 'auto',
+    padding: 30,
+    backgroundColor: colors.white,
+    borderRadius: 8,
+    elevation: 4
+  }
 })
